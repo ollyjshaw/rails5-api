74e894f55b4bbdffbee4ee62448951ffc109f02c
Olly Shaw <ollyjshaw@gmail.com>
2018-07-06 15:30:21 +0100

    some simple test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save a user" do
+    u = User.new(name: "Olly")
+    assert u.save
+  end
+
+  test "using raises" do
+    err = assert_raises(ActiveRecord::RecordInvalid) do
+      u = User.new
+      u.save!
+    end
+    assert_equal "Validation failed: Name can't be blank", err.message
+  end
+
+  test "using raises naa" do
+    err = assert_raises(ActiveRecord::RecordInvalid) do
+      u = User.new
+      u.save!
+    end
+    assert_equal "Validation failed: Name can't be blank", err.message
+  end
 end
